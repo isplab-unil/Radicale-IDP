a0144b43472e34c3e20d27560ba64f764bdcbcb1
Rémy Marquis <remy.marquis@heig-vd.ch>
2025-05-16 09:53:58 +0200

    Move salt from env var to Radicale config

--- radicale/config.py
+++ radicale/config.py
@@ -816,7 +816,12 @@ This is an automated message. Please do not reply.""",
         ("max_freebusy_occurrence", {
             "value": "10000",
             "help": "number of occurrences per event when reporting",
-            "type": positive_int})]))
+            "type": positive_int})])),
+    ("privacy", OrderedDict([
+        ("salt", {
+            "value": "default_salt_change_me",
+            "help": "salt used for hashing privacy identifiers",
+            "type": str})]))
     ])
 
 
