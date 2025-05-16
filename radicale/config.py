918ef9a4cf20769ffccc68d5a5933e616330a7cb
Rémy Marquis <remy.marquis@heig-vd.ch>
2025-05-16 11:21:23 +0200

    Adjust config params acess and default value for privacy_folder

--- radicale/config.py
+++ radicale/config.py
@@ -823,8 +823,8 @@ This is an automated message. Please do not reply.""",
             "help": "salt used for hashing privacy identifiers",
             "type": str}),
         ("privacy_folder", {
-            "value": ".Radicale.privacy",
-            "help": "folder name for storing privacy settings",
+            "value": "/var/lib/radicale/privacy",
+            "help": "folder for storing privacy settings (absolute path)",
             "type": str})]))
     ])
 
