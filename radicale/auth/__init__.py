f69fbeaeae0b648b7776cad59d7647b697b6b5b1
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-09-09 09:56:58 +0200

    refactor(radicale): simplify auth

--- radicale/auth/__init__.py
+++ radicale/auth/__init__.py
@@ -47,8 +47,8 @@ INTERNAL_TYPES: Sequence[str] = ("none", "remote_user", "http_x_remote_user",
                                  "imap",
                                  "oauth2",
                                  "pam",
-                                 "dovecot",
-                                 "token")
+                                 "dovecot"
+                                )
 
 CACHE_LOGIN_TYPES: Sequence[str] = (
                                     "dovecot",
