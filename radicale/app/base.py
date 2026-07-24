Kozea/Radicale

Log privacy settings during initialization

radicale/app/base.py
@@ -22,7 +22,7 @@ import sys
 import xml.etree.ElementTree as ET
 from typing import Optional
 
-from radicale import (auth, config, hook, httputils, pathutils, rights,
+from radicale import (auth, config, hook, httputils, pathutils, privacy, rights,
                       storage, types, web, xmlutils)
 from radicale.log import logger
 
@@ -38,6 +38,7 @@ class ApplicationBase:
     _storage: storage.BaseStorage
     _rights: rights.BaseRights
     _web: web.BaseWeb
+    _privacy: privacy.PrivacySettings
     _encoding: str
     _permit_delete_collection: bool
     _permit_overwrite_collection: bool
@@ -49,6 +50,7 @@ class ApplicationBase:
         self._storage = storage.load(configuration)
         self._rights = rights.load(configuration)
         self._web = web.load(configuration)
+        self._privacy = privacy.load(configuration)
         self._encoding = configuration.get("encoding", "request")
         self._log_bad_put_request_content = configuration.get("logging", "bad_put_request_content")
         self._response_content_on_debug = configuration.get("logging", "response_content_on_debug")

isplab-unil/Radicale-IDP

radicale/app/base.py
@@ -23,8 +23,8 @@ import unicodedata
 import xml.etree.ElementTree as ET
 from typing import Optional, Union
 
-from radicale import (auth, config, hook, httputils, log, pathutils, rights,
-                      sharing, storage, types, utils, web, xmlutils)
+from radicale import (auth, config, hook, httputils, log, pathutils, privacy,
+                      rights, sharing, storage, types, utils, web, xmlutils)
 from radicale.log import logger
 from radicale.rights import intersect
 
@@ -120,6 +120,7 @@ class ApplicationBase:
     _rights: rights.BaseRights
     _web: web.BaseWeb
     _sharing: sharing.BaseSharing
+    _privacy: privacy.PrivacySettings
     _encoding: str
     _max_resource_size: int
     _permit_delete_collection: bool
@@ -136,6 +137,7 @@ class ApplicationBase:
         self._rights = rights.load(configuration)
         self._web = web.load(configuration)
         self._sharing = sharing.load(configuration)
+        self._privacy = privacy.load(configuration)
         self._encoding = configuration.get("encoding", "request")
         self._log_bad_put_request_content = configuration.get("logging", "bad_put_request_content")
         self._response_content_on_debug = configuration.get("logging", "response_content_on_debug")